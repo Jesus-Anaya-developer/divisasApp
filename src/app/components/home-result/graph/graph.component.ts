954356ceb80ab741a79cdc04d1e32b12fe087eb2
Jesus Anaya <jesus.anaya.developer@gmail.com>
2023-12-24 19:06:23 -0600

    maquetado grafica

--- src/app/components/home-result/graph/graph.component.ts
+++ src/app/components/home-result/graph/graph.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Chart, ChartType } from 'chart.js/auto';
 
 @Component({
   selector: 'app-graph',
@@ -9,4 +10,28 @@ import { Component } from '@angular/core';
 })
 export class GraphComponent {
 
+  public chart: Chart | undefined;
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+
+    // datos
+    const data = {
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      datasets: [{
+        label: 'My First Dataset',
+        data: [65, 59, 80, 81, 56, 55, 40],
+        fill: false,
+        borderColor: '#68da3e',
+        tension: 0.1
+      }]
+    };
+    // Creamos la gráfica
+    this.chart = new Chart("chart", {
+      type: 'line' as ChartType, // tipo de la gráfica
+      data: data // datos
+    });
+
+  }
 }
